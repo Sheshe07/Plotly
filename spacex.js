84c9138181ae19ea44a31eba515a7b7e75fd8f51
Sheshe07 <79114781+Sheshe07@users.noreply.github.com>
2021-06-22 19:35:03 -0400

    Add files via upload

---
+++ spacex.js
@@ -0,0 +1,38 @@
+//12.3.2
+
+const url = "https://api.spacexdata.com/v2/launchpads";
+d3.json(url).then();
+
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+researcher1 = {
+    name: 'Roza',
+    age: 34,
+    hobby: 'Hiking'
+};
+console.log(Object.keys(researcher1));
+console.log(Object.entries(researcher1));
+
+researcher1 = [['name', 'Roza'], ['age', 34], ['hobby','Hiking']];
+researcher1.forEach(([first, second]) => console.log(first  + ": " + second));
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
